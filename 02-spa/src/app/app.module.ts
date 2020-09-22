20833476ff8d42d02492666ad7b33c28a8589acb
OKYSoft Developer <developer@MacBook-Pro-de-OKYSoft-3.local>
2020-09-22 16:15:49 -0500

    self

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
