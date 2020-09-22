59bcc79a3c679f6031b12047a4a4e27bf1a2fd50
OKYSoft Developer <developer@MacBook-Pro-de-OKYSoft-3.local>
2020-09-22 16:48:55 -0500

    Start with routing

---
+++ 02-spa/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const appRouting = RouterModule.forRoot(ROUTES);
